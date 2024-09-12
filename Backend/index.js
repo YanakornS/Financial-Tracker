3a07b4d1819afdce5f874bbb0ad4fad0feae2a3c
YanakornS <654259002@webmail.npru.ac.th>
2024-09-12 16:05:59 +0700

    Financial_Context

--- Backend/index.js
+++ Backend/index.js
@@ -1,30 +1,28 @@
 const express = require("express");
 const app = express();
-const financialRouter = require ("./routers/financial.router")
+const financialRouter = require("./routers/financial.router");
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
-const cors = require ("cors");
+const cors = require("cors");
+const ORIGIN = process.env.ORIGIN;
 
-
+// ตั้งค่า CORS
 const corsOptions = {
-    origin :"http://localhost:5173"
+  origin: ORIGIN, // ใช้ค่าจาก .env หรือค่าเริ่มต้น
 };
 
 //use Middleware
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(express.urlencoded ({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-
-app.get("/",(req,res) =>{
-    res.send("<h1>Financial Tracker API </h1>")
+app.get("/", (req, res) => {
+  res.send("<h1>Financial Tracker API </h1>");
 });
 
 //Use router
 app.use("/api/vi/financial", financialRouter);
 
-
-
-app.listen(PORT, () =>{
-    console.log("Listening to http://localhost:" + PORT);
-})
+app.listen(PORT, () => {
+  console.log("Listening to http://localhost:" + PORT);
+});
